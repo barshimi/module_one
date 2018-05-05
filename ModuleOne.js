e429cd9fe9e2a27428abd376128ec8412233d761
Shimi Bar <shimi.b@zooz.com>
2018-05-05 18:56:00 +0300

    initial module one

---
+++ ModuleOne.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class ModuleOne extends React.Component {
+  render () {
+    return (
+      <div>
+        <div className='header' style={{textAlign: 'center', height: '100vh'}}>
+          module one page
+        </div>
+      </div>
+    )
+  }
+}
